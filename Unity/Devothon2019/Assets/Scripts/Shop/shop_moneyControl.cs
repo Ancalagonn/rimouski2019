183311e5d984caafaeaf39206df529f56503fec1
Guillaume Gagnon <guillaume-gagnon@live.ca>
2019-03-09 08:18:16 -0500

    sélection des canons

---
+++ Unity/Devothon2019/Assets/Scripts/Shop/shop_moneyControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.UI;   
+
+public class shop_moneyControl : MonoBehaviour
+{
+    public static shop_moneyControl instance;
+
+    public Text moneyDisplay;
+    private void Awake()
+    {
+        instance = this;
+    }
+    void Start()
+    {
+        AfficherArgent();
+    }
+
+
+    public static void AfficherArgent()
+    {
+        float money = PlayerInstance.playerCash;
+        money = Mathf.Round(money * 100f) / 100f;
+        instance.moneyDisplay.text = "Argent : " + money.ToString() + " écu ";
+    }
+
+    public static bool Transaction(float p_money)
+    {
+        
+        if (PlayerInstance.playerCash + p_money < 0)
+        {
+            //il n'y a rien qui se fait puisqu'on serait en négatif
+            return false;
+        }
+        else
+        {
+            PlayerInstance.playerCash += p_money;
+
+            //On affiche le changement de prix
+            AfficherArgent();
+            return true;
+        }
+    }
+}
