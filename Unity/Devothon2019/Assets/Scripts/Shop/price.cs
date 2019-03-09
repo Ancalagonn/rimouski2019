8159b688d6d8d3bf8521c127d8f1afbc0de6cea1
Guillaume Gagnon <guillaume-gagnon@live.ca>
2019-03-09 08:47:53 -0500

    prix

---
+++ Unity/Devothon2019/Assets/Scripts/Shop/price.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class price : MonoBehaviour
+{
+    public static int Canon_Normal = 40;
+    public static int Canon_Triple = 40;
+    public static int Canon_Flame = 40;
+
+    public static int UpgradeCoque = 100;
+    public static int RepaireCoque = 100;
+    public static int AddCrew = 100;
+}
