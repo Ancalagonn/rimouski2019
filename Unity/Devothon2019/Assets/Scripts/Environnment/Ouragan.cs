eecbce6332491715d1fc88b42cf938e69f1196e1
unknown <friendise@live.ca>
2019-03-09 07:45:49 -0500

    change name scene dave

---
+++ Unity/Devothon2019/Assets/Scripts/Environnment/Ouragan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ouragan : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 Pos = this.transform.position;
+
+        Pos.x += Mathf.Pow(10, 2);
+        Pos.y += 10;
+    }
+}
