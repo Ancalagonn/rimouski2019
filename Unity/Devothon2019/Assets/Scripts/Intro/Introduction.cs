456b45e0ce510d8acae33b638359fab0f149bf3c
maxlajoie99 <max.lajoie99@hotmail.com>
2019-03-09 13:13:25 -0500

    ASNIMA

--- Unity/Devothon2019/Assets/Scripts/Intro/Introduction.cs
+++ Unity/Devothon2019/Assets/Scripts/Intro/Introduction.cs
@@ -7,7 +7,8 @@ public class Introduction : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Narrator.SayTextStatic(false, "C'est l'histoire de ma vie");
-        Narrator.SayTextStatic(false, "Gay as fuck");
+        Narrator.SayTextStatic(false, "Au lendemain de la capitulation de Ville-Marie, quatres aventuriers français sont déterminés à libérer la Nouvelle-France de l'envahisseur Anglais.");
+        Narrator.SayTextStatic(false, "Notre capitaine François et ses trois acolytes Pierre, Jacques et Charles, longeront la rive du fabuleux Fleuve St-Laurent, voie maritime importante de la colonie, afin de libérer les grandes villes au prise des Anglais.");
+        Narrator.SayTextStatic(false, "Leur aventure ne se terminera pas avant une traversée de l'Atlantique pour anéantir totalement l'Angleterre.");
     }
 }
