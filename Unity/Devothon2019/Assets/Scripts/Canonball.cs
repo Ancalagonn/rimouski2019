cece9b63bc918b11fba050a591fdfc5b51c8d803
Sypher0rax <42678306+Sypher0rax@users.noreply.github.com>
2019-03-09 10:50:08 -0500

    Basic scene

--- Unity/Devothon2019/Assets/Scripts/Canonball.cs
+++ Unity/Devothon2019/Assets/Scripts/Canonball.cs
@@ -12,18 +12,48 @@ public class Canonball : MonoBehaviour
 
     private bool hasCollided = false;
 
-    public void InitCanonball(Vector3 p_dir, float p_damage, string p_collidingTag)
+    private float maxLifetime = 0;
+    private float currentLifetime = 0;
+
+    AnimationCurve sizeCurve;
+
+    bool hasDropBelowWater = false;
+
+    public void InitCanonball(Vector3 p_dir, float p_damage, string p_collidingTag, float p_lifeTime)
     {
         direction = p_dir;
         damage = p_damage;
         collidingTag = p_collidingTag;
-        Destroy(gameObject, 5f);
+        Destroy(gameObject, p_lifeTime);
+        maxLifetime = p_lifeTime;
+
+        transform.position = direction * 0.5f;
+
+        float initialSize = transform.localScale.x;
+
+        Keyframe[] keys = new Keyframe[] {
+            new Keyframe(0, initialSize),
+            new Keyframe(p_lifeTime * 0.25f, initialSize * 1.35f),
+            new Keyframe(p_lifeTime * 0.50f, initialSize * 1.5f),
+            new Keyframe(p_lifeTime * 0.75f, initialSize * 1.35f),
+            new Keyframe(p_lifeTime * 0.90f, initialSize * 0.95f) };
+
+        sizeCurve = new AnimationCurve(keys);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += direction * speed * Time.deltaTime;
+        currentLifetime += Time.deltaTime;
+
+        if(currentLifetime >= maxLifetime * 0.90f && !hasDropBelowWater && !hasCollided)
+        {
+            hasDropBelowWater = true;
+            //Instantiate Sploush in water
+        }
+
+        transform.localScale = Vector3.one * sizeCurve.Evaluate(currentLifetime);
     }
 
     private void OnTriggerEnter2D(Collider2D col)
