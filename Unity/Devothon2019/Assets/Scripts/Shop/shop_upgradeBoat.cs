e4e21d910c5d78a5d14e62aa23b3aee78b351dbb
maxlajoie99 <max.lajoie99@hotmail.com>
2019-03-10 10:13:33 -0400

    FUASJI

--- Unity/Devothon2019/Assets/Scripts/Shop/shop_upgradeBoat.cs
+++ Unity/Devothon2019/Assets/Scripts/Shop/shop_upgradeBoat.cs
@@ -24,6 +24,7 @@ public class shop_upgradeBoat : MonoBehaviour
         if(shop_moneyControl.Transaction(-shop_text.repairPrice))
         {
             PlayerInstance.playerStats.currentHp = PlayerInstance.playerStats.maxHp;
+            shop_text.repairPrice = 0;
         }
     }
 
