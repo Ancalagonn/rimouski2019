312be0ada9205c95bed181d6fe73d3913805c6e4
Sypher0rax <42678306+Sypher0rax@users.noreply.github.com>
2019-03-10 07:05:02 -0400

    Hp display

--- Unity/Devothon2019/Assets/Scripts/ShowInfo.cs
+++ Unity/Devothon2019/Assets/Scripts/ShowInfo.cs
@@ -6,12 +6,47 @@ using UnityEngine.UI;
 public class ShowInfo : MonoBehaviour
 {
     public Text cashText;
-    public Text hpText;
+    public Transform hpImageParent;
+
+    private List<Image> hpDisplay = new List<Image>();
+
+    private void Start()
+    {
+        foreach (Transform child in hpImageParent)
+        {
+            hpDisplay.Add(child.GetComponent<Image>());
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
         cashText.text = PlayerInstance.playerCash + "$";
-        hpText.text = PlayerInstance.playerStats.PercentHpLeft() + "%";
+        //hpText.text = PlayerInstance.playerStats.PercentHpLeft() + "%";
+        CalculateHp();
+
+    }
+
+    void CalculateHp()
+    {
+        int percent = (int)(PlayerInstance.playerStats.PercentHpLeft() / 10) - 1;
+
+        if(percent <= 0 && !PlayerInstance.playerStats.isDead())
+        {
+            percent = 1;
+        }
+
+        for (int i = 0; i < hpDisplay.Count; i++)
+        {
+            if(i <= percent)
+            {
+                hpDisplay[i].color = Color.blue;
+            }
+            else
+            {
+                hpDisplay[i].color = Color.red;
+            }
+        }
+
     }
 }
